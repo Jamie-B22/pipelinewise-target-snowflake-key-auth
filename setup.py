e502c3c8ec295b34be7d92e823f923fa88fa1ec7
Jamie-B22 <78746325+Jamie-B22@users.noreply.github.com>
2025-04-30 16:16:08 +0200

    Update boto3 version

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(name="pipelinewise-target-snowflake-key-auth",
           'snowflake-connector-python[pandas]==3.15.*',
           'inflection==0.5.1',
           'joblib==1.2.0',
-          'boto3==1.23.10',
+          'boto3==1.24.*',
           'numpy==1.26.4',
       ],
       extras_require={
